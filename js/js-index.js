0773e69166ceaccb6197e487d64c2c2626b1a115
GonzaloRosendo <grosendo@hotmail.com>
2022-01-27 13:20:08 -0300

    Update 2

--- js/js-index.js
+++ js/js-index.js
@@ -181,17 +181,17 @@ window.addEventListener('scroll', () => scrollAppear("menuSeries", "menuSeriesPo
 window.addEventListener('scroll', () => scrollAppear("borderCreator", "borderCreatorPos", "borderCreatorOn"));
 
 //--------------------------------------------------SHOW WHEN LOADED-------------------------------------------------- 
-// document.onreadystatechange = function() {
-// 	if (document.readyState !== "complete") {
-// 		document.querySelector(
-// 		  "body").style.visibility = "hidden";
-// 		document.querySelector(
-// 		  "#loader").style.visibility = "visible";
-// 	} else {
-// 		document.querySelector(
-// 		  "#loader").style.display = "none";
-// 		document.querySelector(
-// 		  "body").style.visibility = "visible";
-// 	}
-// };
+document.onreadystatechange = function() {
+	if (document.readyState !== "complete") {
+		document.querySelector(
+		  "body").style.visibility = "hidden";
+		document.querySelector(
+		  "#loader").style.visibility = "visible";
+	} else {
+		document.querySelector(
+		  "#loader").style.display = "none";
+		document.querySelector(
+		  "body").style.visibility = "visible";
+	}
+};
 
